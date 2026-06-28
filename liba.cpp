c12ab9f07129f46fc358c733dd78500c67339c0b
Micheal Abler <email@michael-abler.de>
2026-06-28 13:01:12 +0200

    Initial commit

---
+++ liba.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "liba.h"
+#include "../libf/libf.h"  // Include parent depf header
+
+std::string depa() {
+    std::string result = "... called A ";
+    result += depf();
+    return result;
+}
+
